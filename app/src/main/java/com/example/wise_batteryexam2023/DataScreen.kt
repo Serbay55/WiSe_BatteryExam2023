45d19ab8c02b8f9fee3248c434e29834d712b4a8
Lev Likhanov <lev.likhanov@yahoo.de>
2023-01-25 15:53:11 +0100

    Add DataScreen base

---
+++ app/src/main/java/com/example/wise_batteryexam2023/DataScreen.kt
@@ -0,0 +1,32 @@
+package com.example.wise_batteryexam2023
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DataScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "DataScreenPlaceholder",
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DataScreenPreview () {
+    DataScreen()
+}
