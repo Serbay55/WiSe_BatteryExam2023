20c1a28176880b77a2e5547811ff6105d9e995af
Lev Likhanov <lev.likhanov@yahoo.de>
2023-01-30 00:41:43 +0100

    fixed some warnings und updated dependencies

--- app/src/main/java/com/example/wise_batteryexam2023/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/wise_batteryexam2023/ui/screens/MainScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,7 +17,6 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.wise_batteryexam2023.BottomBarScreen
 import com.example.wise_batteryexam2023.SetupNavGraph
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
--- app/src/main/java/com/example/wise_batteryexam2023/ui/theme/Color.kt
+++ app/src/main/java/com/example/wise_batteryexam2023/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val DarkPrimary = Color(0xFF000000)
 val BrightSecondary = Color(0xFFFFFFFF)
 val BlueTertiary = Color(0xFF00FFFF)
 
+/*
+//alternate colors
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+*/
