fb86b56b9281b718c55568604f3220cf97d81b85
Lev Likhanov <lev.likhanov@yahoo.de>
2023-01-30 16:14:53 +0100

    improved selected Screen visibility and improved back stack handling

--- app/src/main/java/com/example/wise_batteryexam2023/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/wise_batteryexam2023/ui/screens/MainScreen.kt
@@ -2,9 +2,7 @@ package com.example.wise_batteryexam2023.ui.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
+import androidx.compose.material.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -70,8 +69,12 @@ fun RowScope.AddItem(
         selected = currentDestination?.hierarchy?.any{
             it.route == screen.route
         } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
         onClick = {
-            navController.navigate(screen.route)
+            navController.navigate(screen.route){
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
         }
     )
 }
